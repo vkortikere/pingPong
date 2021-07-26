5aa6a895fc603249461e13b6a7416cb500cfc874
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-07-26 09:10:48 -0400

    Add files via upload

--- AI-PING-PONG/main.js
+++ AI-PING-PONG/main.js
@@ -23,8 +23,19 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-}
-
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
 
 function draw(){
 
